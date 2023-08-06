8168f28601411424167d528873abe170116a4c6d
devcrack <aurelio.hdz@yandex.com>
2023-08-06 01:39:13 -0600

    Created main components and initial elements of the ToDo app.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>Acerca de nuestra Aplicación TODO en Vue.js: Empoderando tu Productividad con Elegancia
+    </h1>
+    <p>
+En el mundo moderno, donde la gestión del tiempo es crucial y las tareas se acumulan rápidamente, presentamos con orgullo nuestra aplicación TODO desarrollada en Vue.js. Diseñada con una combinación perfecta de funcionalidad poderosa y una interfaz de usuario impresionante, nuestra aplicación TODO está destinada a elevar tu productividad diaria a nuevas alturas mientras disfrutas de una experiencia intuitiva y atractiva.
+
+Características Destacadas:
+
+Interfaz Visualmente Atractiva:
+Nuestra aplicación TODO es un ejemplo asombroso de cómo la elegancia puede combinarse con la funcionalidad. Cada detalle de la interfaz ha sido meticulosamente diseñado para brindarte una experiencia visualmente agradable. Los colores suaves y el diseño limpio crean un ambiente relajante mientras te concentras en organizar tus tareas.
+
+Gestión de Tareas Simplificada:
+Organizar tus tareas nunca ha sido tan sencillo. Con nuestra aplicación TODO, puedes agregar, editar y eliminar tareas con solo unos pocos clics. La función de arrastrar y soltar te permite reorganizar tus tareas en el orden que desees, brindándote un control completo sobre tu lista de tareas pendientes.
+
+Notificaciones y Recordatorios:
+Nunca vuelvas a perderte una tarea importante. Nuestra aplicación está equipada con un sistema de notificaciones y recordatorios para mantenerte al tanto de tus responsabilidades. Configura recordatorios personalizados y recibe alertas en el momento justo para asegurarte de que ninguna tarea importante se quede atrás.
+
+Compatibilidad Multiplataforma:
+Lleva tu lista de tareas contigo a donde quiera que vayas. Nuestra aplicación TODO es compatible con dispositivos móviles, tabletas y computadoras de escritorio, lo que te permite acceder a tus tareas y mantener tu productividad sin importar dónde te encuentres.
+
+Personalización sin Límites:
+Creemos que cada usuario es único, por lo que hemos incorporado opciones de personalización para adaptar la aplicación según tus preferencias. Cambia el tema, ajusta las preferencias de notificación y configura tu espacio de trabajo de la manera que mejor te funcione.
+
+Optimización de Rendimiento:
+Hemos empleado técnicas de optimización avanzadas para garantizar que la aplicación funcione sin problemas, incluso cuando tu lista de tareas está en constante crecimiento. La velocidad y la eficiencia son fundamentales para que tu experiencia sea fluida y agradable.
+
+Soporte Continuo:
+Nuestro compromiso no termina con la descarga de la aplicación. Estamos dedicados a brindar un soporte continuo para garantizar que tu experiencia sea siempre excepcional. Escuchamos tus comentarios y estamos comprometidos en mejorar constantemente la aplicación para satisfacer tus necesidades cambiantes.
+
+En resumen, nuestra aplicación TODO en Vue.js es mucho más que una simple herramienta de gestión de tareas. Es una compañera confiable que te ayudará a conquistar tus responsabilidades diarias con gracia y estilo. Únete a nosotros en este emocionante viaje hacia la productividad elevada y experimenta la diferencia que una aplicación bien diseñada puede marcar en tu vida.
+    </p>
   </div>
 </template>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,15 +1,52 @@
 <template>
-  <hello-world />
+  <div class="home">
+     <v-list
+      class="pt-0"
+         flat
+    >
+       <div
+       v-for="task in tasks"
+        :key="task.id">
+         <v-list-item>
+          <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{task.title}}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+         <v-divider></v-divider>
+       </div>
+    </v-list>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
   export default {
     name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+    data() {
+      return {
+        tasks:[
+          {
+            id: 1,
+            title: "Wake Up"
+          },
+          {
+            id: 2,
+            title: "Get Bananas"
+          },
+          {
+            id: 3,
+            title: "Eat Bananas"
+          }
+        ]
+      }
+    }
   }
 </script>
